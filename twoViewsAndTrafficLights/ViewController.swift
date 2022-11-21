854e29f391cf82b1fc0ad0a3dd390fbce76d993a
Roman <bbromka27@gmail.com>
2022-11-21 23:03:30 +0500

    full

--- twoViewsAndTrafficLights/ViewController.swift
+++ twoViewsAndTrafficLights/ViewController.swift
@@ -20,21 +20,41 @@ class ViewController: UIViewController {
     
     private var currentLight = CurrenLight.red
     private let lightIsOn: CGFloat = 1
-    private let LightIsOff: CGFloat = 0.3
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redView.layer.cornerRadius = 45
-        yellowView.layer.cornerRadius = 45
-        greenView.layer.cornerRadius = 45
         button.layer.cornerRadius = 12
         
-        redView.alpha = LightIsOff
-        yellowView.alpha = LightIsOff
-        greenView.alpha = LightIsOff 
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
         // Do any additional setup after loading the view.
     }
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
 
-
+    @IBAction func startButton() {
+        button.setTitle("Next", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowView.alpha = lightIsOff
+            greenView.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
 }
 
