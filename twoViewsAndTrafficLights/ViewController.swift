dd308462a863b6b691163d931171b16ce2a19c20
Roman <bbromka27@gmail.com>
2022-11-21 18:02:58 +0500

    add views and button

--- twoViewsAndTrafficLights/ViewController.swift
+++ twoViewsAndTrafficLights/ViewController.swift
@@ -7,10 +7,31 @@
 
 import UIKit
 
+enum CurrenLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var button: UIButton!
+    
+    private var currentLight = CurrenLight.red
+    private let lightIsOn: CGFloat = 1
+    private let LightIsOff: CGFloat = 0.3
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redView.layer.cornerRadius = 45
+        yellowView.layer.cornerRadius = 45
+        greenView.layer.cornerRadius = 45
+        button.layer.cornerRadius = 12
+        
+        redView.alpha = LightIsOff
+        yellowView.alpha = LightIsOff
+        greenView.alpha = LightIsOff 
         // Do any additional setup after loading the view.
     }
 
